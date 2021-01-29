b57fdbfb48d78776050cf4ef416d59e35bd7a3fb
Eva Garcia <emgg2222@gmail.com>
2021-01-29 13:46:55 +0100

    validation error

--- app.js
+++ app.js
@@ -29,13 +29,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * API URL
+ */
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/products', productsRouter);
+
+
+/**
+ * Application URL
+ */
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/products', productsRouter)
 app.use('/product', productRouter)
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,22 +53,27 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
 
+  // validation error
+  if(err.array) {
+    const errorInfo = err.array({onlyFirstError: true})[0];
+    err.message = `Not valid - ${errorInfo.param} ${errorInfo.msg}`;
+    err.status = 422;
+  }
   
   res.status(err.status || 500);
   if(isApiRequest(req))
   {
     res.json ({error: err.message});
-    returnM
-
+    return;
   }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  
-
   res.render('error');
+
 });
 
 function isApiRequest(req){
